cea9e6a7d61d2ed16c35d3643fc12d88622ed5a4
salEZY <bobicaleks@yahoo.com>
2020-08-31 14:40:43 +0200

    refactor user modal

--- true-recycler-front/src/components/UserModal/UserModal.js
+++ true-recycler-front/src/components/UserModal/UserModal.js
@@ -15,8 +15,20 @@ const UserModal = ({ HideUserModalHandler }) => {
       <span onClick={HideUserModalHandler}>
         <i className="fas fa-window-close" aria-hidden="true" title="Close"></i>
       </span>
-      <div className="user-info"></div>
-      <div className="change-password"></div>
+      <div className="user-info">
+        <h4>User info</h4>
+        <p>User e-mail: {auth.email}</p>
+        <p>Joined: {auth.joined.split("T")[0].replace(/-/g, ".")}</p>
+      </div>
+      <div className="change-password">
+        <h4>Change password</h4>
+        <form>
+          <input type="text" placeholder="Old Password"></input>
+          <input type="text" placeholder="New Password"></input>
+          <input type="text" placeholder="Repeat new Password"></input>
+          <button>Change!</button>
+        </form>
+      </div>
       <button className="auth" onClick={logoutHandler}>
         Logout <i className="fas fa-sign-out-alt" aria-hidden="true"></i>
       </button>
