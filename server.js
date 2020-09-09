ed31741ea867bed7c8520662fe80809b373129f8
AleksBobic <bobicaleks@yahoo.com>
2020-09-09 21:40:32 +0200

    refactor for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const port = process.env.PORT || 8085;
 
 const connectDb = require("./helpers/db");
@@ -14,6 +15,17 @@ app.use(cors());
 
 app.use("/api/materials/", require("./routes/materials"));
 app.use("/api/auth/", require("./routes/users"));
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("true-recycler-front/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "true-recycler-front", "build", "index.html")
+    );
+  });
+}
 
 app.listen(port, () => {
   console.log(`TrueRecycler server started at port ${port}`);
